a81ad38f7a2cb067f3266c40a9ef5e475b32c5a8
Kerem Apaydın <104307028+keremapaydin@users.noreply.github.com>
2022-06-27 17:00:45 +0300

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,43 @@
+export const robots =[
+    {
+        id: 1,
+        name: 'Kerem Apaydın', 
+        email: 'kerem.apaydin@hotmail.com'
+    },
+    {
+        id: 2,
+        name: 'Erdem Bozdağ',
+        email: 'erdem.bozdag@hotmail.com'
+    },
+    {
+        id: 3,
+        name: 'Yetkin Ünal', 
+        email: 'yetkin.unal@hotmail.com'
+    },
+    {
+        id: 4,
+        name: 'Burak Çeltik',
+        email: 'burak.celtik@hotmail.com'
+    },
+    {
+        id: 5,
+        name: 'Gökalp Okumuş',
+        email: 'gokalp.okumus@hotmail.com'
+    },
+
+    {
+        id: 6,
+        name: 'Seda Erbaş',
+        email: 'seda.erbas@hotmail.com'
+    },
+    {
+        id: 7,
+        name: 'Cemhan Erilmez',
+        email: 'cemhan.erilmez@hotmail.com'
+    },
+    {
+        id: 8,
+        name: 'Aysu Ulaş',
+        email: 'aysu.ulas@hotmail.com'
+    },
+]
